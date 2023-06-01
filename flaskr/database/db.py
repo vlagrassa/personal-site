2aa76e4e7c66c97a1d672fc72c6402f2a43dc588
Vince LaGrassa <vincent.lagrassa@gmail.com>
2023-05-31 23:37:01 -0500

    Make basic project db classes and page

--- flaskr/database/db.py
+++ flaskr/database/db.py
@@ -1,7 +1,53 @@
+import enum
+
 import sqlalchemy as sa
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+#
+# Projects
+#
+
+class ProjectRole(enum.Enum):
+  PERSONAL = 'Personal'       # Self-driven project
+  LEAD     = 'Project Lead'
+  MEMBER   = 'Team Member'
+  SOLO     = 'Solo Project'   # e.g. an assignment
+
+class ProjectLocation(enum.Enum):
+  INT = 'Internal'            # Web app hosted on this site
+  EXT = 'External'            # Link to an external site
+  IRL = 'IRL'                 # Description of a project without a web presence
+
+class Project(db.Model):
+  id       = sa.Column(sa.String(25), primary_key=True)
+  title    = sa.Column(sa.String(50))
+  role     = sa.Column(sa.Enum(ProjectRole))
+  location = sa.Column(sa.Enum(ProjectLocation))
+  created_date  = sa.Column(sa.Date)
+  modified_date = sa.Column(sa.Date)
+
+class TagType(enum.Enum):
+  LANGUAGE = 'Language'
+  SKILL    = 'Skill'
+  TOOL     = 'Tool'
+
+class ProjectTag(db.Model):
+  id = sa.Column(sa.Integer, primary_key=True)
+  title = sa.Column(sa.String)
+  tagtype = sa.Column(sa.Enum(TagType))
+
+class ProjectTagMap(db.Model):
+  id = sa.Column(sa.Integer, primary_key=True)
+  project_id = sa.Column(sa.Integer, sa.ForeignKey('project.id'),     nullable=False)
+  tag_id     = sa.Column(sa.Integer, sa.ForeignKey('project_tag.id'), nullable=False)
+
+
+#
+# Blog Posts
+#
 
 class Post(db.Model):
   id = sa.Column(sa.String(50), primary_key=True)
--- flaskr/views/projects.py
+++ flaskr/views/projects.py
@@ -1,15 +1,32 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+  Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
 from ..constants import *
+from ..database import Project, ProjectTag, ProjectTagMap, ProjectLocation
 
 projects_bp = Blueprint('projects', __name__, url_prefix='/projects')
 
 
+
+def parse_project_object(project):
+  return {
+    'id':   project.id,
+    'name': project.title,
+    'created_date':  project.created_date  or None,
+    'modified_date': project.modified_date or None,
+    'role':     project.role,
+    'location': project.location,
+  }
+
+
 @projects_bp.route('')
 def projects():
-  return render_template('placeholder.html', **{
+  return render_template('projects.html', **{
     'title': PAGE_TITLES[1]['title'],
     'lang': 'en',
+    'ProjectLocation': ProjectLocation,
+    'projects': [
+      parse_project_object(p) for p in Project.query.all()
+    ],
   })
