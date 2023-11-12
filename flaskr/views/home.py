ea2e5266266036f9e927a97c16570f6ef34d53f0
Vince LaGrassa <vincent.lagrassa@gmail.com>
2023-11-11 19:00:06 -0600

    Parse using classes instead of single function
    Different classes control different levels of the document, & formalize
    the fields available.
    
    Bundles different translations of the same paragraph together.

--- flaskr/views/home.py
+++ flaskr/views/home.py
@@ -4,7 +4,7 @@ from flask import (
 
 from ..constants import *
 from ..utils.image import HeaderImage
-from ..utils.parse import parse_content_md
+from ..utils.parse import TextDocument
 from ..utils.utils import nb
 
 home_bp = Blueprint('home', __name__, url_prefix='')
@@ -36,5 +36,5 @@ def home():
       'tj': 'Vincent LɒGrɒssɒ',
     },
     'header_image': HeaderImage('images/home-bg.jpg', location='static', x_align='right'),
-    'content': list(parse_content_md('home')),
+    'content': TextDocument.parse_file('home'),
   })
