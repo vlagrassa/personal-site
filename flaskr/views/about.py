797fd55f65f4852ccdde39e005d6d55c5621a4ba
Vince LaGrassa <vincent.lagrassa@gmail.com>
2024-03-10 23:27:25 -0500

    Add programming langs to new langs panel

--- flaskr/views/about.py
+++ flaskr/views/about.py
@@ -1,3 +1,5 @@
+import csv
+
 from flask import (
   Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify, current_app,
 )
@@ -8,6 +10,15 @@ about_bp = Blueprint('about', __name__, url_prefix='/about')
 
 @about_bp.route('')
 def about():
+  try:
+    with current_app.open_resource(f'static/about-data-languages.csv', mode='rt') as csvfile:
+      data_languages = [
+        {'label': line[0], 'value': int(line[1])} for line in csv.reader(csvfile)
+      ]
+  except Exception as e:
+    data_languages = None
+
   return render_template('about-me.html', **{
     'title': current_app.config['PAGES']['about']['title'],
+    'data_languages':  data_languages
   })
