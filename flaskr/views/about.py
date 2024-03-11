369cae5568a6354891a8d3c0a958181bcce82afb
Vince LaGrassa <vincent.lagrassa@gmail.com>
2024-03-10 22:00:37 -0500

    Add About Me page template

--- flaskr/views/about.py
+++ flaskr/views/about.py
@@ -8,6 +8,6 @@ about_bp = Blueprint('about', __name__, url_prefix='/about')
 
 @about_bp.route('')
 def about():
-  return render_template('placeholder.html', **{
+  return render_template('about-me.html', **{
     'title': current_app.config['PAGES']['about']['title'],
   })
