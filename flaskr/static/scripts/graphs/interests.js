2cf8a9b6b3876eb5f34a2496afcde3841cd6261e
Vince LaGrassa <vincent.lagrassa@gmail.com>
2024-07-03 19:21:28 -0500

    Create basic line graph for interests

--- flaskr/static/scripts/graphs/interests.js
+++ flaskr/static/scripts/graphs/interests.js
@@ -1,3 +1,124 @@
-export function graph_svg_interests(container, data) {
-  console.log('graphing interests!')
+/*
+  Inspiration:
+    - https://observablehq.com/@d3/multi-line-chart/2
+*/
+
+
+export function graph_svg_interests(container, {schema, data}) {
+
+  // Mape dates to date objects
+  data = data.map((d) => Object.assign(d, {'date': new Date(d['date'])}));
+
+  const bounding_rect = container.getBoundingClientRect();
+
+  // Specify the chart’s dimensions.
+  const width  = bounding_rect.width;
+  const height = bounding_rect.height;
+
+  const marginLeft   = 25;
+  const marginRight  = 25;
+  const marginTop    = 25;
+  const marginBottom = 25;
+
+  // Create the SVG container
+  const svg = d3.create("svg")
+    .attr('class', 'graph-interests')
+    .attr("width",  '100%')
+    .attr("height", '100%')
+    .attr("viewBox", [0, 0, width, height])
+    .style("font-size", "10px")
+
+  // Create the positional scales.
+  const x = d3.scaleUtc()
+    .domain(d3.extent(data, d => d.date))
+    .range([marginLeft, width - marginRight])
+
+  const y = d3.scaleLinear()
+    .domain([0, 5]).nice()
+    .range([height - marginBottom, marginTop])
+
+
+
+  // Add the horizontal axis.
+  svg.append("g")
+      .attr("transform", `translate(0, ${height - marginBottom})`)
+      .call(d3.axisBottom(x).ticks(width / 80).tickSize(0))
+
+
+  // Add the vertical axis.
+  svg.append("g")
+      .attr("transform", `translate(${marginLeft},0)`)
+      .call(d3.axisLeft(y, 5).ticks(5))
+      .call(g => g.append("text")
+          .attr("x", -marginLeft)
+          .attr("y", 10)
+          .attr("fill", "currentColor")
+          .attr("text-anchor", "start")
+          .text("↑ Interest (%)"))
+
+
+  const points = data.map((d) => [ x( d.date ), y( d.value ), d.id ])
+
+  // Group the points by series.
+  const groups = d3.rollup(points, v => Object.assign(v, {z: v[0][2]}), d => d[2]);
+
+  // Draw the lines.
+  const line = d3.line()
+  const path = svg.append("g")
+      .attr("fill", "none")
+      .attr("stroke", "var(--color-secondary-1)")
+      .attr("stroke-width", 1.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+    .selectAll("path")
+    .data(groups.values())
+    .join("path")
+      .style("mix-blend-mode", "multiply")
+      .attr("d", line)
+
+  // Add an invisible layer for the interactive tip.
+  const dot = svg.append("g")
+      .attr("display", "none")
+
+  dot.append("circle")
+      .attr("r", 2.5)
+  dot.append("text")
+      .attr("text-anchor", "middle")
+      .attr("y", -8)
+
+  svg
+      .on("pointerenter", pointerentered)
+      .on("pointermove", pointermoved)
+      .on("pointerleave", pointerleft)
+      .on("touchstart", event => event.preventDefault())
+
+
+  return svg;
+
+
+
+  // When the pointer moves, find the closest point, update the interactive tip, and highlight
+  // the corresponding line. Note: we don't actually use Voronoi here, since an exhaustive search
+  // is fast enough.
+  function pointermoved(event) {
+    const [xm, ym] = d3.pointer(event);
+    const i = d3.leastIndex(points, ([x, y]) => Math.hypot(x - xm, y - ym));
+    const [x, y, k] = points[i];
+    path.style("stroke", ({z}) => z === k ? null : "#ddd").filter(({z}) => z === k).raise();
+    dot.attr("transform", `translate(${x},${y})`);
+    dot.select("text").text(k);
+    svg.property("value", data[i]).dispatch("input", {bubbles: true});
+  }
+
+  function pointerentered() {
+    path.style("mix-blend-mode", null).style("stroke", "#ddd");
+    dot.attr("display", null);
+  }
+
+  function pointerleft() {
+    path.style("mix-blend-mode", "multiply").style("stroke", null);
+    dot.attr("display", "none");
+    svg.node().value = null;
+    svg.dispatch("input", {bubbles: true});
+  }
 }
